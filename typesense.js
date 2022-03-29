6d911353a20bdc65133d681e0313718302257a46
Nyior <nyior.clement@aun.edu.ng>
2022-03-29 07:21:47 +0100

    initial commit :)

---
+++ typesense.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+import { Client } from 'typesense';
+
+// Create a new client
+const client = new Client({
+	nodes: [
+	  {
+			host: process.env.TYPESENSE_HOST,
+			port: process.env.TYPESENSE_PORT,
+			protocol: process.env.TYPESENSE_PROTOCOL,
+	  },
+	],
+	apiKey: process.env.TYPESENSE_ADMIN_KEY,
+	connectionTimeoutSeconds: 2,
+});
+
+export default client;
